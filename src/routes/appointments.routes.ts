4b822e3246f32186970b867566e54cfd71ea5892
Nilton Pedro <niltonpedro@MacBook-Air-de-Nilton.local>
2020-06-19 19:33:59 -0300

    Time validate

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,23 +1,40 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentRouter.get('/', (request, response) => {
-  return response.json({
-    message: 'deubom',
-  });
+  return response.json(appointments);
 });
 
 appointmentRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parseDate = startOfHour(parseISO(date));
+
+  const findAppointmentSameDate = appointments.find(appointment =>
+    isEqual(parseDate, appointment.date),
+  );
+
+  if (findAppointmentSameDate) {
+    return response.status(400).json({
+      message: 'Horário já reservado',
+    });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parseDate,
   };
 
   appointments.push(appointment);
